b655ede9b8e79c2859d552865faabf8e8e07e048
loihehe <112553641+loihehe@users.noreply.github.com>
2022-10-19 23:48:43 +0700

    Add files via upload
    20-10

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thiên Minh';
+const giftUrl = 'https://www.facebook.com/builoi1606';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Ebe Của Mình <3';
+const contentCard = 'Bé ơi! Hôm nay là một ngày ý nghĩa, mong rằng không những chỉ có ngày hôm nay mà tất cả những tháng ngày về sau, mỗi ngày đều sẽ là một ngày vui vẻ và hạnh phúc. Hiện tại và sau này, cho dù có nhau hay không thì vẫn mong rằng, cuộc sống sẽ đối xử với công chúa của a nhẹ nhàng một xíu nữa, để bé không phải nhận sự tổn thương thêm bất kỳ một lần nào nữa. "20-10 hạnh phúc nhé" ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
